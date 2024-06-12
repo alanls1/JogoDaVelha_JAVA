08baf2a63a4117dcf2d395a81a9e61aab1bf478f
alanls1 <alanlima4123@gmail.com>
2024-06-12 20:13:56 -0300

    Jogo da Velha em Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import jogoDaVelha.jogoDaVelha;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+         jogoDaVelha jogoVelha = new jogoDaVelha();
+         jogoVelha.JogoDaVelha();
+        boolean[] click = new boolean[9];
+
+    }
+}
